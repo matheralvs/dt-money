7f1bcaf52b6ff2033384e7f4ea7cfc6bca98028e
Matheus Alves <mtalves27@gmail.com>
2024-04-22 10:33:21 -0300

    feat: added json server online

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://my-json-server.typicode.com/matheralvs/ControlFinance/";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: apiUrl,
 });
